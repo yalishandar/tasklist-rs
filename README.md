f3e441e0e1b99f5af415777227df0a4c451052dc
tkos <108073828+yalishandar@users.noreply.github.com>
2022-06-24 07:33:17 +0800

    more changes

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # tasklist-rs
+## a crate let you can easily get process name or process id on windows
+
+- based on [`windows-rs`](https://github.com/microsoft/windows-rs) crate 
+
+
+```
+use tasklist;
+
+fn main(){
+    unsafe{
+        //get a HashMap<String,u32> of the tasklist
+        let list = tasklist::tasklist();
+        println!("{:#?}",list);
+
+        //find the process name by id
+        let pname = tasklist::find_process_name_by_id(9720);
+        println!("{:#?}",pname);
+
+        //find first process id by name
+        let pid = tasklist::find_first_process_id_by_name("cmd.exe");
+        println!("{:#?}",pid);
+
+        //find process id by name
+        let aid = tasklist::find_process_id_by_name("cmd.exe");
+        println!("{:#?}",aid);
+    }
+    
+
+}
+```
+
+## Usage
+
+Add this to your `Cargo.toml`:
+
+```toml
+[dependencies]
+tasklist = "0.1.1"
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
+//! # tasklist
+//! 
+//! `tasklist` is a crate let you easily find process name of process id on windows.
+//! it based on [`windows-rs`](https://github.com/microsoft/windows-rs) crate.
+
 use std::mem::zeroed;
+use std::collections::HashMap;
 use windows::Win32::Foundation::CHAR;
 use windows::Win32::Foundation::CloseHandle;
 use std::mem::size_of;
 use windows::Win32::System::Diagnostics::ToolHelp::{CreateToolhelp32Snapshot,TH32CS_SNAPPROCESS,PROCESSENTRY32,Process32First,Process32Next};
 
-
-pub unsafe fn find_all_process_id(process_name:&str)->Vec<i32>{
-    let mut temp:Vec<i32> = vec![];
+///find the process id by the name you gave , it return a `Vec<U32>` , if the process is not exist , it will return a empty `Vec<u32>`
+/// ```
+/// unsafe{
+///     let aid = tasklist::find_process_id_by_name("cmd.exe");
+///     println!("{:#?}",aid);
+/// }
+/// ```
+pub unsafe fn find_process_id_by_name(process_name:&str)->Vec<u32>{
+    let mut temp:Vec<u32> = vec![];
     let h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0).unwrap();
 
     let mut process =zeroed::<PROCESSENTRY32>();
@@ -17,7 +29,7 @@ pub unsafe fn find_all_process_id(process_name:&str)->Vec<i32>{
 
             if Process32Next(h, &mut process).as_bool(){
                 if get_proc_name(process.szExeFile) == process_name {
-                    temp.push(process.th32ProcessID.try_into().unwrap());
+                    temp.push(process.th32ProcessID);
                 }
             }else{
                 break;
@@ -30,7 +42,13 @@ pub unsafe fn find_all_process_id(process_name:&str)->Vec<i32>{
 
 }
 
-pub unsafe fn find_first_process_id(process_name:&str)->i32{
+/// return the first process id by the name you gave , it return the `Option<u32>` , `u32` is the process id.
+/// ```
+/// unsafe{
+///     let pid = tasklist::find_first_process_id_by_name("cmd.exe");
+///     println!("{:#?}",pid);
+/// }
+pub unsafe fn find_first_process_id_by_name(process_name:&str)->Option<u32>{
 
         let h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0).unwrap();
 
@@ -39,18 +57,30 @@ pub unsafe fn find_first_process_id(process_name:&str)->i32{
 
         if Process32First(h,&mut process).as_bool(){
             loop{
-                Process32Next(h, &mut process);
-                if get_proc_name(process.szExeFile) == process_name {
-                    break;
+
+                if Process32Next(h, &mut process).as_bool(){
+                    if get_proc_name(process.szExeFile) == process_name {
+                        break;
+                    }
+                }else{
+                    return None;
                 }
             }
         }
 
         CloseHandle(h);
-        process.th32ProcessID.try_into().unwrap()
+        Some(process.th32ProcessID)
 }
 
-pub unsafe fn find_process_name(process_id:i32)->String{
+/// just like the name , this function will return a `Option<String>` by the id you gave, `String` is the name of process.
+/// ```
+/// unsafe{
+///     let pname = tasklist::find_process_name_by_id(9720);
+///     println!("{:#?}",pname);
+/// }
+/// 
+/// ```
+pub unsafe fn find_process_name_by_id(process_id:u32)->Option<String>{
 
     let h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0).unwrap();
 
@@ -59,19 +89,57 @@ pub unsafe fn find_process_name(process_id:i32)->String{
 
     if Process32First(h,&mut process).as_bool(){
         loop{
-            Process32Next(h, &mut process);
-            let id:i32 = process.th32ProcessID.try_into().unwrap();
-            if  id == process_id{
-                break;
+
+            if Process32Next(h, &mut process).as_bool(){
+                let id:u32 = process.th32ProcessID;
+                if id == process_id {
+                    break;
+                }
+            }else{
+                return None;
             }
         }
     }
 
     CloseHandle(h);
     
-    get_proc_name(process.szExeFile)
+    Some(get_proc_name(process.szExeFile))
 }
 
+
+/// get the windows tasklist ,return a `HashMap<String,u32>`
+/// `String` is the name of process, and `u32` is the id of process
+/// ```
+/// unsafe{
+///     let list = tasklist::tasklist();
+///     println!("{:#?}",list);
+/// }
+/// ```
+pub unsafe fn tasklist()->HashMap<String,u32>{
+    let mut temp:HashMap<String, u32> = HashMap::new();
+
+    let h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0).unwrap();
+
+    let mut process =zeroed::<PROCESSENTRY32>();
+    process.dwSize= size_of::<PROCESSENTRY32>() as u32;
+
+    if Process32First(h,&mut process).as_bool(){
+        loop{
+
+            if Process32Next(h, &mut process).as_bool(){
+                temp.insert(get_proc_name(process.szExeFile), process.th32ProcessID.try_into().unwrap());
+            }else{
+                break;
+            }
+        }
+    }
+
+    CloseHandle(h);
+    temp
+}
+
+
+///get the proc name by windows `[CHAR;260]` , retun the `String` name for human.
 fn get_proc_name(name:[CHAR;260])->String{
 
     let mut temp:Vec<u8> = vec![];
